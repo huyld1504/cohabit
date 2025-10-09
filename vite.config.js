37576a9960f686baf36dad52f1d21f62a898366a
huyld1504 <ledinhhuy0105@gmail.com>
2025-10-09 07:49:45 +0700

    fix deploy app

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      },
-      output: {
-        manualChunks: {
-          react: ['react', 'react-dom'],
-          router: ['react-router-dom'],
-          redux: ['@reduxjs/toolkit', 'react-redux'],
-        }
-      }
-    },
     sourcemap: false,
     minify: 'terser',
-    target: 'es2018'
+    target: 'es2018',
+    commonjsOptions: {
+      include: [/node_modules/],
+      transformMixedEsModules: true
+    }
+  },
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' }
   },
   optimizeDeps: {
-    include: ['react', 'react-dom', 'react-router-dom']
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src')
+    esbuildOptions: {
+      target: 'es2018'
     }
+  },
+  server: {
+    port: 3000
   }
 });
